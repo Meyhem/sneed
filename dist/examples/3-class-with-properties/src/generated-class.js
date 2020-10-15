af2402271b8b1d856432c7a462810e55eab15bbe
meyhem <meyhem@protonmail.com>
2020-10-15 18:44:33 +0200

    removed garbage file

--- dist/examples/3-class-with-properties/src/generated-class.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MyClass = void 0;
-var MyClass = /** @class */ (function () {
-    function MyClass() {
-    }
-    return MyClass;
-}());
-exports.MyClass = MyClass;
